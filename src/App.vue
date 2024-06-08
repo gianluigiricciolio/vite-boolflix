b7df51b209c075df696bbe3d8fa1ad84e7f96410
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-06-08 20:20:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+// import store
+import { store } from './store';
+// import components
+import Component from './components/Component.vue';
+
+export default {
+  data() {
+    return {
+      store
+    }
+  },
+
+  components: {
+    Component,
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+  <h1>Vite Vue Template</h1>
+  <p>template di partenza per progetti Vue</p>
+</template>
+
+<style scoped></style>
---
+++ src/components/Component.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'ComponentName',
+    data() {
+        return {
+
+        }
+    },
+
+    methods: {
+
+    }
+}
+
+</script>
+
+<template></template>
+
+<style scoped></style>
